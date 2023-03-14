d6e521966db34ff801ebf9306c2b5d7ace57c735
Kaoru <96850544+Uvxtq@users.noreply.github.com>
2023-03-14 17:56:59 -0400

    Add files via upload

--- Functions/Knit Helper.lua
+++ Functions/Knit Helper.lua
@@ -1,11 +1,11 @@
 --[[
     -- << Documentation: >> --
-        Helper.new() --> Helper
-        Helper:Load() --> Knit
-        Helper:GetControllers() --> Controllers
-        Helper:GetController(Controller) --> ControllerContents
-        Helper:DumpKnit() --> Dumped to Console
-        Helper:DumpToFile() --> Dumped to KnitHelper.txt
+        Helper.new(); --> Helper
+        Helper:Load(); --> Knit
+        Helper:GetControllers(); --> Controllers
+        Helper:GetController(Controller); --> ControllerContents
+        Helper:DumpKnit(); --> Dumped to Console
+        Helper:DumpToFile(); --> Dumped to KnitHelper.txt
 
     -- << Example: >> --
         local Helper = Helper.new(); --> Creates a new Helper
@@ -13,120 +13,117 @@
         Helper:DumpKnit(); --> Dumps Controllers to the console
         Helper:DumpToFile(); --> Dumps Controllers to KnitHelper.txt
 ]]
---
 
 -- << Modules >>
-local Serialize = loadstring(
-	game:HttpGet("https://raw.githubusercontent.com/Uvxtq/Custom-Lua-Functions/main/Functions/Table%20Serializer.lua")
-)()
+local Serialize = loadstring(game:HttpGet("https://raw.githubusercontent.com/Uvxtq/Custom-Lua-Functions/main/Functions/Table%20Serializer.lua"))();
 
 -- << Services >> --
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local ReplicatedStorage = game:GetService("ReplicatedStorage");
 
 -- << Functions >> --
 local function Concat(Table)
-	local Formatted = {}
-	local Position = 1
+	local Formatted = {};
+	local Position = 1;
 
 	for _, Value in next, Table do
 		if typeof(Value) ~= "string" then continue; end
 		if Value:match("^%s*$") then continue; end
 		if not string.find(Value, "%w") then continue; end
 
-		Formatted[Position] = Value
-		Position += 1
+		Formatted[Position] = Value;
+		Position += 1;
 	end
 
-	return table.concat(Formatted, ", ")
+	return table.concat(Formatted, ", ");
 end
 
 local function Filter(Type, Table)
 	if typeof(Table) ~= "table" then return Table; end
 	if Type == "Constants" then
-		local Constants = {}
+		local Constants = {};
 
 		for _, Constant in next, Table do
 			if typeof(Constant) == "function" then
-				table.insert(Constants, debug.getinfo(Constant).name)
+				table.insert(Constants, debug.getinfo(Constant).name);
 			elseif typeof(Constant) == "table" then
-				table.insert(Constants, string.format("table: {%s}", Concat(Constant)))
+				table.insert(Constants, string.format("table: {%s}", Concat(Constant)));
 			elseif typeof(Constant) == "string" then
-				table.insert(Constants, string.format("%s", Constant))
+				table.insert(Constants, string.format("%s", Constant));
 			else
-				table.insert(Constants, tostring(Constant))
+				table.insert(Constants, tostring(Constant));
 			end
 		end
 
-		return Constants
+		return Constants;
 	end
 
 	if Type == "Upvalues" then
-		local Upvalues = {}
+		local Upvalues = {};
 
 		for _, Upvalue in next, Table do
 			if typeof(Upvalue) == "function" then
-				table.insert(Upvalues, debug.getinfo(Upvalue).name)
+				table.insert(Upvalues, debug.getinfo(Upvalue).name);
 			elseif typeof(Upvalue) == "table" then
-				table.insert(Upvalues, string.format("table: {%s}", Concat(Upvalue)))
+				table.insert(Upvalues, string.format("table: {%s}", Concat(Upvalue)));
 			elseif typeof(Upvalue) == "string" then
-				table.insert(Upvalues, string.format('"%s"', Upvalue))
+				table.insert(Upvalues, string.format('"%s"', Upvalue));
 			else
-				table.insert(Upvalues, tostring(Upvalue))
+				table.insert(Upvalues, tostring(Upvalue));
 			end
 		end
 
-		return Upvalues
+		return Upvalues;
 	end
 
 	if Type == "Protos" then
-		local Protos = {}
+		local Protos = {};
 
 		for _, Proto in next, Table do
 			if typeof(Proto) == "function" then
-				table.insert(Protos, debug.getinfo(Proto).name)
+				table.insert(Protos, debug.getinfo(Proto).name);
 			elseif typeof(Proto) == "table" then
-				table.insert(Protos, string.format("table: {%s}", Concat(Proto)))
+				table.insert(Protos, string.format("table: {%s}", Concat(Proto)));
 			elseif typeof(Proto) == "string" then
-				table.insert(Protos, string.format('"%s"', Proto))
+				table.insert(Protos, string.format('"%s"', Proto));
 			else
-				table.insert(Protos, tostring(Proto))
+				table.insert(Protos, tostring(Proto));
 			end
 		end
 
-		return Protos
+		return Protos;
 	end
 end
 
 local function GetFunctionData(Function)
 	if typeof(Function) ~= "function" then
-		return nil, "Function is not a function"
+		return nil, "Function is not a function";
 	end
 
-	local Info = debug.getinfo(Function)
-	local Protos = debug.getprotos(Function)
-	local Upvalues = debug.getupvalues(Function)
-	local Constants = debug.getconstants(Function)
+	local Info = debug.getinfo(Function);
+	local Protos = debug.getprotos(Function);
+	local Upvalues = debug.getupvalues(Function);
+	local Constants = debug.getconstants(Function);
 
 	return {
 		Info = Info,
 		Protos = Filter("Protos", Protos),
 		Upvalues = Filter("Upvalues", Upvalues),
-		Constants = Filter("Constants", Constants),
+		Constants = Filter("Constants", Constants)
 	}
 end
 
 local function FormatInfo(Function)
 	if typeof(Function) ~= "function" then
-		return nil, "Function is not a function"
+		return nil, "Function is not a function";
 	end
 
-	local Data = GetFunctionData(Function)
+	local Data = GetFunctionData(Function);
 
 	if Data then
-		local Info = Data.Info
-		local Protos = Filter("Protos", Data.Protos)
-		local Upvalues = Filter("Upvalues", Data.Upvalues)
-		local Constants = Filter("Constants", Data.Constants)
+		local Info = Data.Info;
+		local Protos = Filter("Protos", Data.Protos);
+		local Upvalues = Filter("Upvalues", Data.Upvalues);
+		local Constants = Filter("Constants", Data.Constants);
 
 		return string.format(
 			"{\n            Path: %s,\n            Constants: {%s},\n            Upvalues: {%s},\n            Protos: {%s}\n        }",
@@ -137,134 +134,126 @@ local function FormatInfo(Function)
 		)
 	end
 
-	return nil, "Failed to get function data"
+	return nil, "Failed to get function data";
 end
 
 -- << Helper >> --
-local Helper = {}
-do
-	Helper.__index = Helper
+local Helper = {}; do
+	Helper.__index = Helper;
 
 	function Helper.new()
-		local self = setmetatable({}, Helper)
+		local self = setmetatable({}, Helper);
 
-		self.Knit = nil
-		self.Module = nil
+		self.Knit = nil;
+		self.Module = nil;
 
-		self.Controllers = {}
-		self.Services = {}
+		self.Controllers = {};
+		self.Services = {};
 
-		self.ControllerContents = {}
-		self.ServiceContents = {}
+		self.ControllerContents = {};
+		self.ServiceContents = {};
 
-		return self
+		return self;
 	end
 
 	function Helper:GetKnitModule()
 		if self.Module then
-			return self.Module
+			return self.Module;
 		end
 
 		for _, Module in next, ReplicatedStorage:GetDescendants() do
 			if Module:IsA("ModuleScript") and Module.Name == "KnitClient" then
-				self.Module = Module
+				self.Module = Module;
 
-				return self.Module
+				return self.Module;
 			end
 		end
 
-		return nil, "KnitClient not found"
+		return nil, "KnitClient not found";
 	end
 
 	function Helper:Load()
 		local GetKnitModule = self:GetKnitModule()
 
 		if GetKnitModule then
-			self.Knit = require(GetKnitModule)
+			self.Knit = require(GetKnitModule);
 
-			return self.Knit
+			return self.Knit;
 		end
 
-		return nil, "KnitClient not found"
+		return nil, "KnitClient not found";
 	end
 
 	function Helper:GetControllers()
-		local Knit = self.Knit
+		local Knit = self.Knit;
 
 		if Knit then
-			self.Controllers = rawget(Knit, "Controllers")
+			self.Controllers = rawget(Knit, "Controllers");
 
-			return self.Controllers
+			return self.Controllers;
 		end
 
-		return nil, "Knit not found"
+		return nil, "Knit not found";
 	end
 
 	function Helper:GetController(Controller)
-		local Controllers = self:GetControllers()
+		local Controllers = self:GetControllers();
 
 		if Controllers then
-			self.ControllerContents = rawget(Controllers, Controller)
+			self.ControllerContents = rawget(Controllers, Controller);
 
-			return self.ControllerContents
+			return self.ControllerContents;
 		end
 
-		return nil, "Controller not found"
+		return nil, "Controller not found";
 	end
 
 	function Helper:DumpKnit()
-		local Controllers = self:GetControllers()
+		local Controllers = self:GetControllers();
 
 		print("Controllers:")
 		table.foreach(Controllers, function(Index, Value)
-			--local Success, Error = pcall(function()
-			warn("    " .. Index .. ":")
-			table.foreach(Value, function(Index, Value)
-				if Index == "Name" then
-					return
-				end
-				if typeof(Value) == "function" then
-					print("        " .. Index .. ": " .. FormatInfo(Value))
-					return
-				end
-				print("        " .. Index .. ": " .. tostring(Value))
+			local Success, Error = pcall(function()
+                warn("    " .. Index .. ":")
+                table.foreach(Value, function(Index, Value)
+                    if Index == "Name" then return; end
+                    if typeof(Value) == "function" then
+                        print("        " .. Index .. ": " .. FormatInfo(Value));
+                        return;
+                    end
+                    print("        " .. Index .. ": " .. tostring(Value));
+                end)
 			end)
-			--end)
 
-			--if not Success then
-			-- warn('Failed to dump controller "'..Index..'"');
-			--end
+			if not Success then
+			    warn('Failed to dump controller "'..Index..'"');
+			end
 		end)
 	end
 
 	function Helper:DumpToFile()
-		local Controllers = self:GetControllers()
+		local Controllers = self:GetControllers();
 
-		writefile("KnitHelper.txt", "Controllers:\n")
+		writefile("KnitHelper.txt", "Controllers:\n");
 
 		table.foreach(Controllers, function(Index, Value)
 			local Success, Error = pcall(function()
-				appendfile("KnitHelper.txt", "    " .. Index .. ":\n")
+				appendfile("KnitHelper.txt", "    " .. Index .. ":\n");
 				table.foreach(Value, function(Index, Value)
-					if Index == "Name" then
-						return
-					end
+					if Index == "Name" then return; end
 					if typeof(Value) == "function" then
-						appendfile("KnitHelper.txt", "        " .. Index .. ": " .. FormatInfo(Value) .. "\n")
-						return
+						appendfile("KnitHelper.txt", "        " .. Index .. ": " .. FormatInfo(Value) .. "\n");
+						return;
 					end
-					appendfile("KnitHelper.txt", "        " .. Index .. ": " .. tostring(Value) .. "\n")
+					appendfile("KnitHelper.txt", "        " .. Index .. ": " .. tostring(Value) .. "\n");
 				end)
 			end)
 
 			if not Success then
-				warn('Failed to dump controller "' .. Index .. '" to file')
+				warn('Failed to dump controller "' .. Index .. '" to file');
 			end
 		end)
 	end
 end
 
-local Helper = Helper.new() --> Creates a new Helper
-Helper:Load() --> Loads Knit (Can also be in a variable to check if it loaded)
-Helper:DumpKnit() --> Dumps Controllers to the console
---Helper:DumpToFile(); --> Dumps Controllers to KnitHelper.txt
+return Helper;
